fb41b617f1c67d87c493f457bf5e8a21a6e35f5e
Juan Pedro <juanpedro.developer@gmail.com>
2023-11-15 19:31:19 -0300

    :sparkles: home page / not found page

--- src/app/views/not-found/not-found.module.ts
+++ src/app/views/not-found/not-found.module.ts
@@ -4,14 +4,18 @@ import { CommonModule } from '@angular/common';
 import { NotFoundRoutingModule } from './not-found-routing.module';
 import { NotFoundComponent } from './not-found.component';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    NotFoundComponent
-  ],
+  declarations: [NotFoundComponent],
   imports: [
     CommonModule,
-    NotFoundRoutingModule
-  ]
+    NotFoundRoutingModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+  ],
 })
-export class NotFoundModule { }
+export class NotFoundModule {}
