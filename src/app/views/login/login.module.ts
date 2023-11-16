043554f3c5d0629e326b28b86f95f96566dc757d
Juan Pedro <juanpedro.developer@gmail.com>
2023-11-15 21:06:33 -0300

    :sparkles: login

--- src/app/views/login/login.module.ts
+++ src/app/views/login/login.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import  {MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
-  ]
+    LoginRoutingModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatInputModule,
+    MatAutocompleteModule
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
